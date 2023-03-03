2782af372f1645d634ece5a9616e5fe119e49521
dengruetf2 <dengruetf2@Admins-MacBook-Air.local>
2023-03-03 19:07:48 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# dengruetf2
---
+++ config.php
@@ -0,0 +1,19 @@
+---
+author: tamram
+ms.service: storage
+ms.topic: include
+ms.date: 11/25/2018
+ms.author: tamram
+ms.openlocfilehash: 1db7be902c768889dbc82bef0adc0310b9fac02c
+ms.sourcegitcommit: 2ec4b3d0bad7dc0071400c2a2264399e4fe34897
+ms.translationtype: HT
+ms.contentlocale: de-DE
+ms.lasthandoff: 03/27/2020
+ms.locfileid: "67177990"
+---
+## <a name="create-an-azure-storage-account"></a>Erstellen eines Azure-Speicherkontos
+Ihr erstes Azure-Speicherkonto erstellen Sie am einfachsten im [Azure-Portal](https://portal.azure.com). Weitere Informationen finden Sie unter [Erstellen von Speicherkonten](https://docs.microsoft.com/azure/storage/common/storage-quickstart-create-account).
+
+Ein Azure-Speicherkonto können Sie auch mit [Azure PowerShell](../articles/storage/common/storage-powershell-guide-full.md), der [Azure-Befehlszeilenschnittstelle](../articles/storage/common/storage-azure-cli.md) oder der [Storage Resource Provider-Clientbibliothek für .NET](https://msdn.microsoft.com/library/azure/mt131037.aspx) erstellen.
+
+
